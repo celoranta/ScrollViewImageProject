13bae15fcfa3b3378ff6fd95d749ac8250cbbc44
Christoper <celoranta@gmail.com>
2018-02-26 12:09:17 -0800

    Initial Commit

---
+++ ScrollViewImageProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageProject
+//
+//  Created by Chris Eloranta on 2018-02-26.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
